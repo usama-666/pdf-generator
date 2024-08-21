308308a49e502d0d65e99c3df08fc4f19c836ade
usama-666 <osamariaz369@gmail.com>
2024-08-21 11:26:44 +0500

    chore:updated pdf-code

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 const express = require("express");
 const puppeteer = require("puppeteer");
+const path = require("path");
+const ejs = require("ejs");
+const fs = require("fs");
 const app = express();
 
 const port = process.env.PORT || 4000;
 
 app.get("/gen-pdf", async (req, res) => {
+    console.log("inside rew");
     try {
-        const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
-        const page = await browser.newPage();
-        await page.setContent("<h1>Hello World</h1>");
-        await page.pdf({ path: "test.pdf", format: "A4" });
-        await browser.close();
+        const templatePath = path.join(__dirname, "medication.ejs");
+        fs.readFile(templatePath, "utf-8", async (err, template) => {
+            if (err) {
+                res.status(500).json({ message: "Error reading template" });
+                return;
+            }
+            const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
+            const filePath = path.join(__dirname, "Medication.pdf");
+            console.log(filePath);
+            const renderedHtml = ejs.render(template);
+            const page = await browser.newPage();
+            await page.setContent(renderedHtml, { waitUntil: "networkidle0" });
+            await page.pdf({ path: filePath, format: "A4" });
 
-        res.json({ message: "Generating PDF" });
+            await browser.close();
+
+            // Send the PDF to the client
+            res.download(filePath, `Medication.pdf`, (err) => {
+                if (err) {
+                    res.status(500).json({ message: err });
+                } else {
+                    console.log("pdf generated", filePath);
+                    fs.unlinkSync(filePath);
+                }
+            });
+        });
     } catch (error) {
         res.json({ message: "Error generating PDF" });
     }
