5c728fc2506532c61d5d8ef9cd6199f57848fac9
usama-666 <osamariaz369@gmail.com>
2024-08-22 14:09:12 +0500

    pdf-generat

--- routes/pdf.route.js
+++ routes/pdf.route.js
@@ -3,8 +3,17 @@ const path = require("path");
 const puppeteer = require("puppeteer");
 const router = express.Router();
 const fs = require("fs");
+
+router.route("/load").get(async (req, res) => {
+    try {
+        const data = { data: "usama riaz fnkjsanfjksdfjnsdkj " };
+        res.render("medication", data);
+    } catch (error) {
+        console.log(error);
+    }
+});
 router.route("/gen-pdf").get(async function (req, res) {
-    const data = { data: "usama riaz " };
+    // const data = { data: "usama riaz " };
 
     // res.render("medication", data, async (err, html) => {
     //     if (err) {
@@ -14,35 +23,40 @@ router.route("/gen-pdf").get(async function (req, res) {
     //             .json({ message: "Error occurred in rendering ejs " });
     //     }
     // });
-    res.render("medication", data, async (err, html) => {
-        if (err) {
-            console.log(err);
-            return res
-                .status(400)
-                .json({ message: "Error occurred in rendering ejs " });
-        }
-        const filePath = path.join(__dirname, "Medication.pdf");
-        console.log(filePath);
-        // const html = ejs.render(template);
-        const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
+    const filePath = path.join(__dirname, "Medication.pdf");
+    console.log(filePath);
+    // const html = ejs.render(template);
+    const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
 
-        const page = await browser.newPage();
-        await page.setContent(html, { waitUntil: "networkidle0" });
-        await page.pdf({ path: filePath, format: "A4" });
+    const page = await browser.newPage();
 
-        await browser.close();
-
-        // Send the PDF to the client
-        res.download(filePath, `Medication.pdf`, (err) => {
-            if (err) {
-                return res.status(500).json({
-                    message: ` Error in downloading pdf:  ${err}`,
-                });
-            } else {
-                console.log("pdf generated ", filePath);
-                fs.unlinkSync(filePath);
-            }
-        });
+    await page.goto("http://localhost:4000/api/load", {
+        waitUntil: "networkidle2",
     });
+    // await page.setContent(html, { waitUntil: "networkidle0" });
+    await page.pdf({ path: filePath, format: "A4" });
+
+    await browser.close();
+
+    // Send the PDF to the client
+    res.download(filePath, `Medication.pdf`, (err) => {
+        if (err) {
+            return res.status(500).json({
+                message: ` Error in downloading pdf:  ${err}`,
+            });
+        } else {
+            console.log("pdf generated ", filePath);
+            fs.unlinkSync(filePath);
+        }
+    });
+    // res.render("medication", data, async (err, html) => {
+    //     if (err) {
+    //         console.log(err);
+    //         return res
+    //             .status(400)
+    //             .json({ message: "Error occurred in rendering ejs " });
+    //     }
+
+    // });
 });
 module.exports = router;
